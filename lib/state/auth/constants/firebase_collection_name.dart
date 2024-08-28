3f6e1e7a6e6384d47ec6d37cc3e160a5f3787f42
hazzem salah <hazzemsalaht6@gmail.com>
2024-08-29 00:29:21 +0300

    created auth_state_notifier & done with store user's info after logging in

---
+++ lib/state/auth/constants/firebase_collection_name.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/foundation.dart' show immutable;
+import 'package:flutter/widgets.dart';
+
+@immutable
+class FirebaseCollectionName {
+  static const thumnails = "thumnails";
+  static const comments = "comments";
+  static const likes = "likes";
+  static const posts = "posts";
+  static const users = "users";
+
+  const FirebaseCollectionName._();
+}
